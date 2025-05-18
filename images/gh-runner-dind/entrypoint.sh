32c0c007822ba25a8ce2c99ebec6f3fe65c419f3
travmonkey <travis@batk.me>
2025-05-18 15:23:25 -0600

    New github runner image

---
+++ images/gh-runner-dind/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+cd /runner
+
+.config.sh --url "REPO_URL" \
+           --token "ACCESS_TOKEN" \
+           --labels "LABELS" \
+           --unattended \
+           --replace
+
+./run.sh
